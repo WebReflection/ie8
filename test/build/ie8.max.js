419fc0e9922d270e959e5fc90663163b302df6c4
Andrea Giammarchi <agiammarchi@twitter.com>
2013-07-19 01:54:55 -0700

    automatic test generator

---
+++ test/build/ie8.max.js
@@ -0,0 +1,267 @@
+/*!
+Copyright (C) 2013 by WebReflection
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+var main = (function(window){
+  /*! (C) WebReflection Mit Style License */
+  if (document.createEvent) return;
+  var
+    DUNNOABOUTDOMLOADED = true,
+    SECRET = '__IE8__' + Math.random(),
+    defineProperties = Object.defineProperties ||
+    // IE8 implemented defineProperty but not the plural...
+    function (object, descriptors) {
+      for(var key in descriptors) {
+        if (hasOwnProperty.call(descriptors, key)) {
+          Object.defineProperty(object, key, descriptors[key]);
+        }
+      }
+    },
+    hasOwnProperty = Object.prototype.hasOwnProperty,
+    ElementPrototype = window.Element.prototype,
+    EventPrototype = window.Event.prototype,
+    DocumentPrototype = window.HTMLDocument.prototype,
+    WindowPrototype = window.Window.prototype
+  ;
+
+  function commonEventLoop(currentTarget, e, $handlers, bubbles) {
+    for(var
+      handler,
+      evt = enrich(e, currentTarget),
+      handlers = $handlers.slice(),
+      i = 0, length = handlers.length; i < length; i++
+    ) {
+      handler = handlers[i];
+      if (
+        typeof handler === 'object' &&
+        typeof handler.handleEvent === 'function'
+      ) {
+        handler.handleEvent(evt);
+      } else {
+        handler.call(currentTarget, evt);
+      }
+      if (evt.stoppedImmediatePropagation) break;
+    }
+    return (
+      bubbles &&
+      !evt.stoppedPropagation &&
+      currentTarget.parentNode
+    ) ?
+      currentTarget.parentNode.dispatchEvent(evt) :
+      !evt.defaultPrevented
+    ;
+  }
+
+  function enrich(e, currentTarget) {
+    if (!e.currentTarget) {
+      e.currentTarget = currentTarget;
+    }
+    e.eventPhase = (
+      // AT_TARGET : BUBBLING_PHASE
+      (e.target || e._target) === e.currentTarget ? 2 : 3
+    );
+    return e;
+  }
+
+  function find(array, value) {
+    var i = array.length;
+    while(i-- && array[i] !== value);
+    return i;
+  }
+
+  function verify(e) {
+    if (!e) {
+      e = window.event;
+    }
+    if (!e.timeStamp) {
+      e.timeStamp = (new Date).getTime();
+    }
+    return e;
+  }
+
+  defineProperties(
+    ElementPrototype,
+    {
+      addEventListener: {value: function (type, handler, capture) {
+        var
+          self = this,
+          ontype = 'on' + type,
+          temple =  self[SECRET] ||
+                      Object.defineProperty(
+                        self, SECRET, {value: {}}
+                      )[SECRET],
+          currentType = temple[ontype] || (temple[ontype] = {}),
+          handlers  = currentType.h || (currentType.h = []),
+          e
+        ;
+        if (!currentType.w) {
+          currentType.w = function (e) {
+            return commonEventLoop(self, verify(e), handlers);
+          };
+          try {
+            e = document.createEventObject();
+            self.cloneNode(true).fireEvent(ontype, e);
+            currentType.n = true;
+            self.attachEvent(ontype, currentType.w);
+          } catch(e) {
+            currentType.n = false;
+          }
+        }
+        if (find(handlers, handler) < 0) {
+          handlers[capture ? 'unshift' : 'push'](handler);
+        }
+      }},
+      dispatchEvent: {value: function (e) {
+        var
+          self = this,
+          ontype = 'on' + e.type,
+          temple =  self[SECRET],
+          currentType = temple && temple[ontype],
+          valid = !!currentType
+        ;
+        return (valid && currentType.n) ?
+          self.fireEvent(ontype, e) : (
+            valid && commonEventLoop(
+              self,
+              (e._target = self) && e,
+              currentType.h,
+              true
+            )
+          );
+      }},
+      removeEventListener: {value: function (type, handler, capture) {
+        var
+          self = this,
+          ontype = 'on' + type,
+          temple =  self[SECRET],
+          currentType = temple && temple[ontype],
+          handlers = currentType && currentType.h,
+          i = handlers ? find(handlers, handler) : -1
+        ;
+        if (-1 < i) handlers.splice(i, 1);
+      }}
+    }
+  );
+
+  defineProperties(
+    EventPrototype,
+    {
+      target: {get: function () {
+        return this.srcElement;
+      }},
+      preventDefault: {value: function () {
+        if (this.cancelable) {
+          this.defaultPrevented = true;
+          this.returnValue = false;
+        }
+      }},
+      stopPropagation: {value: function () {
+        this.stoppedPropagation = true;
+        this.cancelBubble = true;
+      }},
+      stopImmediatePropagation: {value: function () {
+        this.stoppedImmediatePropagation = true;
+        this.stopPropagation();
+      }},
+      initEvent: {value: function(type, bubbles, cancelable){
+        this.type = type;
+        this.bubbles = !!bubbles;
+        this.cancelable = !!cancelable;
+        if (!this.bubbles) {
+          this.stopPropagation();
+        }
+      }}
+    }
+  );
+
+  defineProperties(
+    DocumentPrototype,
+    {
+      addEventListener: {value: function(type, handler, capture) {
+        var self = this;
+        ElementPrototype.addEventListener.call(self, type, handler, capture);
+        if (
+          DUNNOABOUTDOMLOADED &&
+          type === 'DOMContentLoaded' &&
+          !/loaded|complete/.test(
+            self.readyState
+          )
+        ) {
+          DUNNOABOUTDOMLOADED = false;
+          (function gonna(e){try{
+            self.documentElement.doScroll('left');
+            e = self.createEvent('Event');
+            e.initEvent(type, true, true);
+            self.dispatchEvent(e);
+            }catch(o_O){
+            setTimeout(gonna, 50);
+          }}());
+        }
+      }},
+      dispatchEvent: {value: ElementPrototype.dispatchEvent},
+      removeEventListener: {value: ElementPrototype.removeEventListener},
+      createEvent: {value: function(Class){
+        var e;
+        if (Class !== 'Event') throw new Error('unsupported ' + Class);
+        e = document.createEventObject();
+        e.timeStamp = (new Date).getTime();
+        return e;
+      }}
+    }
+  );
+
+  defineProperties(
+    WindowPrototype,
+    {
+      addEventListener: {value: function (type, handler, capture) {
+        var
+          self = window,
+          ontype = 'on' + type,
+          handlers
+        ;
+        if (!self[ontype]) {
+          self[ontype] = function(e) {
+            return commonEventLoop(self, verify(e), handlers);
+          };
+        }
+        handlers = self[ontype][SECRET] || (
+          self[ontype][SECRET] = []
+        );
+        if (find(handlers, handler) < 0) {
+          handlers[capture ? 'unshift' : 'push'](handler);
+        }
+      }},
+      dispatchEvent: {value: function (e) {
+        var method = window['on' + e.type];
+        return method ? method.call(window, e) : false;
+      }},
+      removeEventListener: {value: function (type, handler, capture) {
+        var
+          ontype = 'on' + type,
+          handlers = (window[ontype] || Object)[SECRET],
+          i = handlers ? find(handlers, handler) : -1
+         ;
+        if (-1 < i) handlers.splice(i, 1);
+      }}
+    }
+  );
+
+}(this));;
---
+++ test/test/ie8.js
@@ -0,0 +1,30 @@
+
+document.addEventListener('DOMContentLoaded', function(e){
+  function E(type) {
+    var e = document.createEvent('Event');
+    e.initEvent(type, true, true);
+    return e;
+  }
+  function show(e) {
+    alert([
+      'type: ' + e.type,
+      'timeStamp: ' + e.timeStamp,
+      'eventPhase: ' + e.eventPhase,
+      'e.currentTarget: ' + e.currentTarget
+    ].join('\n'));
+  }
+  show(e);
+  this.addEventListener('custom-event-name', {handleEvent:function (e) {
+    show(e);
+    document.removeEventListener(e.type, this);
+    // won't trigger anyway
+    document.dispatchEvent(E(e.type));
+  }});
+  this.documentElement.addEventListener('custom-event-name', show);
+  this.body.innerHTML = 'OK, now try to click somewhere';
+  this.documentElement.addEventListener('click', function (e) {
+    show(e);
+    this.dispatchEvent(E('custom-event-name'));
+  });
+});
+
